876c8b655c72bdaa82dd48770dbe02f0af541d03
vrindabindal12 <vrindabindal1212@gmail.com>
2025-04-02 22:10:10 +0530

    resume

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,12 +1,9 @@
-import { inngest } from '../../../lib/inngest/client';
-import { helloWorld } from "../../../lib/inngest/functions"
 import { serve } from "inngest/next";
 
+import { inngest } from "../../../lib/inngest/client";
+import { generateIndustryInsights } from "../../../lib/inngest/functions";
+
 export const { GET, POST, PUT } = serve({
-  client: inngest, // a client created with new Inngest()
-  functions: [ /* */ 
-    helloWorld, // an example function that returns "Hello, World!"
-    /* Add more functions here */
-   ], // an array of Inngest functions to serve, created with inngest.createFunction()
-  /* Optional extra configuration */
+  client: inngest,
+  functions: [generateIndustryInsights],
 });
